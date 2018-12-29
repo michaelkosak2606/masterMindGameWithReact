f6ef68aabc821cdbc45cc6b850db22e54bb4b1bc
Michael Kosak <michaelkosak2606@Michaels-MacBook-Pro.local>
2018-12-29 04:12:00 +0100

    Updates

---
+++ precache-manifest.51d140f65028b3b4b1caab7b8b09ed00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8d2da52a75131bcb9186f31d55da247",
+    "url": "/masterMindGameWithReact/static/media/sideBoardOp1.a8d2da52.png"
+  },
+  {
+    "revision": "9be866d0b1bec1eaa33e",
+    "url": "/masterMindGameWithReact/static/js/runtime~main.9be866d0.js"
+  },
+  {
+    "revision": "a35a3e8e97db71a69932",
+    "url": "/masterMindGameWithReact/static/js/main.a35a3e8e.chunk.js"
+  },
+  {
+    "revision": "e9626dcb2cbc708be97e",
+    "url": "/masterMindGameWithReact/static/js/1.e9626dcb.chunk.js"
+  },
+  {
+    "revision": "a35a3e8e97db71a69932",
+    "url": "/masterMindGameWithReact/static/css/main.4416d9b3.chunk.css"
+  },
+  {
+    "revision": "03f4bf47ded4eebdfd09b681fba64db8",
+    "url": "/masterMindGameWithReact/index.html"
+  }
+];
